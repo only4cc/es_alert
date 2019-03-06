d21d5f4421932bb2624c64ba10a82d5ec7b21499
only4cc@hotmail.com <only4cc@hotmail.com>
2019-03-06 15:25:43 -0300

    se agrega GUI a crea_variable.py

--- crea_variable.py
+++ crea_variable.py
@@ -1,18 +1,33 @@
+import easygui
 import variable
 
-tenant  = 'BE'          
-varname = 'alguna_metrica'
+title = 'Creacion de Definiciones de Variables'
+
+tenant       = 'BE'          
+varname      = 'alguna_metrica'
 varname_desc = 'Alguna Metrica de Ejemplo'
-query = 'la consulta aca ...'
-prono_type = 'formula'
-formula = '1000000'
-umbral_type = 'percent' 
+query        = 'la consulta aca ...'
+prono_type   = 'F|D - F:Formula| D:Discreto' 
+formula      = '1000000'
+umbral_type  = 'percent' 
 umbral_factor_1 = 1.001
 umbral_factor_2 = 1.010
-lapso = (15*60)
+lapso        = (15*60)
 
-var = variable.Variable(tenant, varname)
-res = var.create_criterio(varname_desc, query, prono_type, formula, lapso, \
-                            umbral_type, umbral_factor_1, umbral_factor_2)   
+fieldNames = [ 'tenant', 'varname', 'varname_desc', 'query', 'prono_type', \
+               'formula', 'umbral_type', 'umbral_factor_1', 'umbral_factor_2' ]
 
-print(res)
+fieldValues = [ tenant, varname, varname_desc, query, prono_type, \
+                formula, umbral_type, umbral_factor_1, umbral_factor_2 ]
+
+
+errmsg = ""
+fieldValues = easygui.multenterbox(errmsg, title, fieldNames, fieldValues)
+
+print ("La respuesta fue:", fieldNames, fieldValues)
+
+#var = variable.Variable(tenant, varname)
+#res = var.create_criterio(varname_desc, query, prono_type, formula, lapso, \
+#                            umbral_type, umbral_factor_1, umbral_factor_2)   
+
+#print(res)
