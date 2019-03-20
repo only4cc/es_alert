550e82544e043e05a58a28c89599f1cc6f2bb800
only4cc@hotmail.com <only4cc@hotmail.com>
2019-03-20 11:12:15 -0300

    Comentario

--- call_evalua.sh
+++ call_evalua.sh
@@ -1,4 +1,5 @@
-# Parametros tenant varname
+# Parametros: 
+# tenant varname
 
 docker run --rm --network="host" --name es_alert_2 -v /home/es_alert/cfg/config.yml:/src/config.yml:ro \
 es_alert:latest python eval_2.py ES tot_docs
